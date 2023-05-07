39a90f5e7c3dc108275f8116005a03a5f5344411
hariomfw21 <darkphinixgamer@gmail.com>
2023-05-07 10:31:54 +0530

    day-4 some added in the doctor route

--- Routes/DoctorRouter.js
+++ Routes/DoctorRouter.js
@@ -4,6 +4,20 @@ const {DoctorModel}=require('../Models/DoctorModel')
 
 const doctorRouter=express.Router()
 
+// for fetching all doctor in doctor page
+
+doctorRouter.get("/getAllDoc",async(req,res)=>{
+    try {
+        let data = await DoctorModel.find()
+        res.json({msg:"success",data:data});
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+// 
+
+
 doctorRouter.get("/getDocID",async(req,res)=>{
     try { 
             let name=req.headers.name;
